99d37975ef27b93931ca2715515ca0d41820f350
James Thayer <jrthayer@users.noreply.github.com>
2022-01-27 13:40:48 -0800

    Version .1

--- src/data/social.js
+++ src/data/social.js
@@ -6,22 +6,16 @@ export default [
         classes_text: "fab fa-youtube fa-2x",
     },
     {
-        name: "Twitch",
-        link: "https://www.twitch.tv/brettultimus",
-        color: "#6441a5",
-        classes_text: "fab fa-twitch fa-2x",
-    },
-    {
-        name: "Twitter",
-        link: "https://twitter.com/BrettUltimus?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
-        color: "#1da1f2",
-        classes_text: "fab fa-twitter fa-2x",
-    },
-    {
         name: "Patreon",
         link: "https://www.patreon.com/worldofio",
         color: "#f96854",
         classes_text: "fab fa-patreon fa-2x",
+    },
+    {
+        name: "Twitch",
+        link: "https://www.twitch.tv/brettultimus",
+        color: "#6441a5",
+        classes_text: "fab fa-twitch fa-2x",
     },
     {
         name: "Discord",
@@ -30,15 +24,27 @@ export default [
         classes_text: "fab fa-discord fa-2x",
     },
     {
+        name: "Twitter",
+        link: "https://twitter.com/BrettUltimus?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+        color: "#1da1f2",
+        classes_text: "fab fa-twitter fa-2x",
+    },
+    {
         name: "Reddit",
         link: "https://www.reddit.com/r/BrettUltimus/",
         color: "#ff5700",
         classes_text: "fab fa-reddit fa-2x",
     },
     {
+        name: "Merch",
+        link: "https://brettultimus.com/",
+        color: "#000",
+        classes_text: "fas fa-shopping-cart fa-2x",
+    },
+    {
         name: "Wiki",
         link: "https://worldofio.fandom.com/wiki/World_of_Io_Wiki",
-        color: "#000",
+
         classes_text: "fas fa-book-open fa-2x",
     },
 ];
--- src/pages/home.js
+++ src/pages/home.js
@@ -33,14 +33,10 @@ let navigationData = [
         name: "How To Support",
         link: "#howToSupport",
     },
-    {
-        name: "About This Site",
-        link: "#whatIsThisSite",
-    },
-    {
-        name: "Footer",
-        link: "#footer",
-    },
+    // {
+    //     name: "Footer",
+    //     link: "#footer",
+    // },
 ];
 
 import socialData from "../data/social.js";
@@ -84,7 +80,7 @@ const Home = (props) => {
             </ResponsiveSidebar>
 
             <Hero id="hero" background={backgroundImage}>
-                <h1 className="fs-700 color-white">World Of IO</h1>
+                <h1 className="fs-700 color-white shadow-text">World Of IO</h1>
                 <img src={logo} alt="" />
                 <SocialBtn
                     link={introLink.link}
@@ -95,7 +91,7 @@ const Home = (props) => {
             </Hero>
             <Section id="aboutIO">
                 <h2 className="fs-700">About IO</h2>
-                <p>
+                <p className="color-primary-dark">
                     World of IO is a a homebrew D&D setting made by BrettUltimus
                     that has been developing for over 2 years now. The world is
                     heavily influenced by the player's action shaping the
@@ -105,10 +101,18 @@ const Home = (props) => {
                         <div className="header fs-700">660</div>
                         <div className="text">
                             <h3 className="text-header">Hours Of Content</h3>
-                            <p>
+                            <p className="color-primary-dark">
                                 Spanning across 245 videos with an average
                                 length of <span>2 hours 40 mins</span>
                             </p>
+                        </div>
+                    </div>
+
+                    <div className="stat-single">
+                        <div className="header fs-700">90+</div>
+                        <div className="text">
+                            <h3 className="text-header">Player Characters</h3>
+                            <p>Across 71 unique players!</p>
                         </div>
                     </div>
 
@@ -125,35 +129,13 @@ const Home = (props) => {
                     </div>
 
                     <div className="stat-single">
-                        <div className="header fs-700">90+</div>
+                        <div className="header fs-700">12+</div>
                         <div className="text">
-                            <h3 className="text-header">Player Characters</h3>
-                            <p>Across 71 unique players!</p>
-                        </div>
-                    </div>
-
-                    <div className="stat-single">
-                        <div className="header fs-700">3</div>
-                        <div className="text">
-                            <h3 className="text-header"></h3>
-                        </div>
-                    </div>
-                    <div className="stat-single">
-                        <div className="header fs-700">3</div>
-                        <div className="text">
-                            <h3 className="text-header"></h3>
-                        </div>
-                    </div>
-                    <div className="stat-single">
-                        <div className="header fs-700">3</div>
-                        <div className="text">
-                            <h3 className="text-header"></h3>
-                        </div>
-                    </div>
-                    <div className="stat-single">
-                        <div className="header fs-700">3</div>
-                        <div className="text">
-                            <h3 className="text-header"></h3>
+                            <h3 className="text-header">Diverse Regions</h3>
+                            <p>
+                                Ranging from the wide open seas to the Astral
+                                plane itself!
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -163,7 +145,9 @@ const Home = (props) => {
                 bgType="color"
                 bgData="var(--color-primary-light)"
             >
-                <h2 className="fs-700">The History Of IO</h2>
+                <h2 className="fs-700 color-white shadow-text-outline">
+                    The History Of IO
+                </h2>
 
                 <h3>Phase 1</h3>
                 <p>
@@ -178,40 +162,120 @@ const Home = (props) => {
             </Section>
             {/* bgType="image" bgData={backgroundImage} */}
             <Section id="howToSupport">
-                <h2>What is the World of IO?</h2>
-                <p>
-                    World of IO is a a homebrew D&D setting made by
-                    BrettUltimus.
-                </p>
-            </Section>
-            <Section
-                id="whatIsThisSite"
-                bgType="color"
-                bgData="var(--color-primary-light)"
-            >
-                <h2>This is a different section</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Ullam facere, tempora quisquam fugiat illo quas provident,
-                    accusamus debitis harum tenetur magnam voluptatem nemo
-                    soluta molestias. Tempore quisquam illum similique illo?
-                    Nihil ratione earum assumenda, aliquam non omnis facere amet
-                    possimus libero unde provident nobis. Minima dolorum
-                    doloribus commodi quas error voluptas, ut dolores voluptatum
-                    eius esse ipsum repellendus molestias temporibus. Voluptatem
-                    quos excepturi ipsa cumque expedita hic ullam ab?
-                    Consectetur, obcaecati quis exercitationem explicabo, cumque
-                    consequuntur architecto autem animi culpa, ipsum optio quas
-                    expedita rerum iusto dignissimos maxime a harum? Commodi,
-                    magni mollitia doloremque velit tenetur alias magnam eaque
-                    harum quasi hic soluta nobis laborum at ea quae adipisci
-                    animi expedita, iusto non optio reprehenderit id reiciendis.
-                    Eum, ipsum tempora!
-                </p>
+                <h2 className="fs-700">How To Support The World of IO</h2>
+                <div className="support-group">
+                    <a
+                        href="https://www.patreon.com/worldofio"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                    >
+                        <div className="support-single">
+                            <div className="icon">
+                                <i
+                                    class="fab fa-patreon fa-2x"
+                                    style={{ color: "#f96854" }}
+                                ></i>
+                            </div>
+                            <div className="description">
+                                <h3 className="color-black">Patreon</h3>
+                                <p>
+                                    Not only does brett get the largest cut from
+                                    patreon support but you get the most bang
+                                    for your buck.
+                                </p>
+                                <br />
+                                <p>
+                                    Patreon members get many exclusive features
+                                    including:
+                                    <ul>
+                                        <li>Session MP3s</li>
+                                        <li>DM Notes</li>
+                                        <li>Sneak Peaks</li>
+                                    </ul>
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+
+                    <div className="support-single">
+                        <div className="icon">
+                            <i class="fas fa-shopping-cart fa-2x"></i>
+                        </div>
+                        <div className="description">
+                            <h3 className="color-black">Merch Store</h3>
+                            <p>Purchase merch of the wholesome Racc himself.</p>
+                            <br />
+                            <p>
+                                Currently the main focus of the merchandise is a
+                                dnd class series that so far includes fighter
+                                and wizard.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="support-single">
+                        <div className="icon">
+                            <i
+                                class="fab fa-twitch fa-2x"
+                                style={{ color: "#6441a5" }}
+                            ></i>
+                        </div>
+                        <div className="description">
+                            <h3 className="color-black">Twitch</h3>
+                            <p>
+                                Watch the sessions live on twitch! Subscribe,
+                                donate, and participate in the chat.
+                            </p>
+                            <br />
+                            <p>
+                                Weekly stream times can be found in the discord!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="support-single">
+                        <div className="icon">
+                            <i
+                                class="fab fa-youtube fa-2x"
+                                style={{ color: "#c4302b" }}
+                            ></i>
+                        </div>
+                        <div className="description">
+                            <h3 className="color-black">Youtube</h3>
+                            <p>
+                                Watching the previous campaigns on the youtube
+                                channel provides ad revenue and helps spread IO
+                                to new viewers.
+                            </p>
+                            <br />
+                            <p>
+                                A master playlist containing all 245 sessions of
+                                Phase 2 is available for your convenience!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="support-single">
+                        <div className="icon">
+                            <i
+                                class="fab fa-discord fa-2x"
+                                style={{ color: "#7289da" }}
+                            ></i>
+                        </div>
+                        <div className="description">
+                            <h3 className="color-black">Discord</h3>
+                            <p>Join and interact with the amazing discord!</p>
+                            <br />
+                            <p>
+                                Wether you're looking for campaign fanart, a
+                                bingo board to fill out during the game, or just
+                                a place to talk about all things IO you can find
+                                it here!
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </Section>
             <Footer
-                className="color-white"
-                style={{ backgroundImage: `url(${backgroundImage})` }}
+                // className="color-white"
+                style={{ backgroundColor: `var(--color-primary-light)` }}
                 id="footer"
             ></Footer>
         </Fragment>
