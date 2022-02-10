92123b03ad4de332db9fe8b88cf7562fd19d56c9
James Thayer <jrthayer@users.noreply.github.com>
2022-02-10 02:08:39 -0800

    Update NavBar to scroll to top of page on active link click

--- src/layout/navBar/navBar.js
+++ src/layout/navBar/navBar.js
@@ -11,12 +11,24 @@ const NavBar = () => {
         <nav className={styles.navBar}>
             <ul>
                 <li>
-                    <NavLink to="/" className={isActive}>
+                    <NavLink
+                        to="/"
+                        className={isActive}
+                        onClick={() => {
+                            window.scrollTo(0, 0);
+                        }}
+                    >
                         Home
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/phase2" className={isActive}>
+                    <NavLink
+                        to="/phase2"
+                        className={isActive}
+                        onClick={() => {
+                            window.scrollTo(0, 0);
+                        }}
+                    >
                         Phase 2 Summary
                     </NavLink>
                 </li>
