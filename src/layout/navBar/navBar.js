d77bcd0de5735a5dd1fdc0f3cec6a6d95fac9488
James Thayer <jrthayer@users.noreply.github.com>
2022-02-10 22:53:06 -0800

    Update navBar styles

--- src/layout/navBar/navBar.js
+++ src/layout/navBar/navBar.js
@@ -1,40 +1,48 @@
-import { Link, NavLink, withRouter } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import styles from "./navBar.module.scss";
 
+import { useScroll } from "components/providers/scrollProvider";
+import { useViewport } from "components/providers/viewportProvider";
+
 function isActive({ isActive }) {
-    return isActive ? styles.activeLink : "";
+    let classes = styles.navLink;
+    if (isActive) {
+        classes += ` ${styles.activeLink}`;
+    }
+    return classes;
 }
 
-const NavBar = () => {
-    return (
-        <nav className={styles.navBar}>
-            <ul>
-                <li>
-                    <NavLink
-                        to="/"
-                        className={isActive}
-                        onClick={() => {
-                            window.scrollTo(0, 0);
-                        }}
-                    >
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/phase2"
-                        className={isActive}
-                        onClick={() => {
-                            window.scrollTo(0, 0);
-                        }}
-                    >
-                        Phase 2 Summary
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    );
+const navJSX = (
+    <nav className={styles.navBar}>
+        <ul className={styles.linkContainer}>
+            <NavLink
+                to="/"
+                className={isActive}
+                onClick={() => {
+                    window.scrollTo(0, 0);
+                }}
+            >
+                <li>Home</li>
+            </NavLink>
+
+            <li>
+                <NavLink
+                    to="/phase2"
+                    className={isActive}
+                    onClick={() => {
+                        window.scrollTo(0, 0);
+                    }}
+                >
+                    Phase 2 Summary
+                </NavLink>
+            </li>
+        </ul>
+    </nav>
+);
+
+const NavBar = (props) => {
+    return navJSX;
 };
 
 export default NavBar;
