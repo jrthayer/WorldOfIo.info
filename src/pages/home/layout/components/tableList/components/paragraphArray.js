26e01e60d2489af93d6beeb2b9edcd4f4cf49675
James Thayer <jrthayer@users.noreply.github.com>
2022-02-16 00:39:53 -0800

    Testing Paragraph Array

--- src/pages/home/layout/components/tableList/components/paragraphArray.js
+++ src/pages/home/layout/components/tableList/components/paragraphArray.js
@@ -11,13 +11,13 @@ const ParagraphArray = (props) => {
     if (Array.isArray(data)) {
         return (
             <Fragment>
-                {data.map((singleEntry, index) => {
+                {/* {data.map((singleEntry, index) => {
                     return (
                         <p key={index} className={styles.paragraph}>
                             {singleEntry}
                         </p>
                     );
-                })}
+                })} */}
             </Fragment>
         );
     } else {
