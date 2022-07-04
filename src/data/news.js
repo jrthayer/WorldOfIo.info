f21ce069d3ba9e6580a659e930278fddf9b94247
James Thayer <jrthayer@users.noreply.github.com>
2022-07-04 08:23:58 -0700

    Update news

--- src/data/news.js
+++ src/data/news.js
@@ -1,36 +1,42 @@
 export default [
     {
-        header: "Phase 3 Groups Announced",
-        sections: [
-            {
-                header: "Deadbeats",
-                info: [
-                    "Players: Dodger, SummersSalt, MasaeAnela, Joefudge, Snake, JesseCox",
-                    "Weekly Schedule: Thursdays 4pm EST",
-                    "Session 0: Thursday, February 10th, 4:00pm EST",
-                    "Session 1: Thursday, February 17th, 2022 4:00pm EST",
-                ],
-            },
-            {
-                header: "Pipe Dreamers",
-                info: [
-                    "Players: Lawlman, Tomato, Layna Lazar, Shaynehawke, Juniper",
-                    "Weekly Schedule: Thursdays 8pm EST",
-                    "Session 0: Tuesday, February 15, 2022 8:00pm EST",
-                    "Session 1:  Thursday, March 3, 2022 4:00pm EST",
-                ],
-            },
-            {
-                header: "Curious Curios",
-                info: [
-                    "Players: Breebunn, KingBendrick, Strippin, Arex, Gmart, Supershigi",
-                    "Weekly Schedule: Tuesdays 7pm EST",
-                    "Session 0: Tuesday, February 22, 2022 7:00pm EST",
-                    "Session 1: Tuesday, March 8, 2022 7:00pm EST",
-                ],
-            },
-        ],
+        header: "Deadbeats Season 2 currently scheduled to start July 21st!",
     },
+    {
+        header: "Curious Curios is BACK this Tuesday at 7pm EST!",
+    },
+    // {
+    //     header: "Phase 3 Groups Announced",
+    //     sections: [
+    //         {
+    //             header: "Deadbeats",
+    //             info: [
+    //                 "Players: Dodger, SummersSalt, MasaeAnela, Joefudge, Snake, JesseCox",
+    //                 "Weekly Schedule: Thursdays 4pm EST",
+    //                 "Session 0: Thursday, February 10th, 4:00pm EST",
+    //                 "Session 1: Thursday, February 17th, 2022 4:00pm EST",
+    //             ],
+    //         },
+    //         {
+    //             header: "Pipe Dreamers",
+    //             info: [
+    //                 "Players: Lawlman, Tomato, Layna Lazar, Shaynehawke, Juniper",
+    //                 "Weekly Schedule: Thursdays 8pm EST",
+    //                 "Session 0: Tuesday, February 15, 2022 8:00pm EST",
+    //                 "Session 1:  Thursday, March 3, 2022 4:00pm EST",
+    //             ],
+    //         },
+    //         {
+    //             header: "Curious Curios",
+    //             info: [
+    //                 "Players: Breebunn, KingBendrick, Strippin, Arex, Gmart, Supershigi",
+    //                 "Weekly Schedule: Tuesdays 7pm EST",
+    //                 "Session 0: Tuesday, February 22, 2022 7:00pm EST",
+    //                 "Session 1: Tuesday, March 8, 2022 7:00pm EST",
+    //             ],
+    //         },
+    //     ],
+    // },
     // {
     //     header: "this is a test",
     //     // sections: [],
