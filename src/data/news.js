440db0e6a3e433f8e04ff4c21cf5d16b1b7f421f
James Thayer <jrthayer@users.noreply.github.com>
2022-08-08 15:41:10 -0700

    Update news

--- src/data/news.js
+++ src/data/news.js
@@ -1,12 +1,15 @@
 export default [
     {
+        header: "The new Paladin Subclass - Oath of the Scholar - is now out on Patreon!",
+    },
+    {
         header: "The new Druid Subclass - Circle of Abnormality - is now out on Patreon!",
     },
     {
         header: "The FREE Raccling Race has been re-released on Patreon. They've been re-adjusted to be compatible with all 5e systems and now have new art by Mariah Tekulve.",
     },
     {
-        header: "Sounds of IOverse Vol. 1 - 10 tracks from the IOverse soundtrack - release date planned for August 7th at 12pm EST!",
+        header: "Sounds of IOverse Vol. 1 - 10 tracks from the IOverse soundtrack - is Now Released! Check the discord announcements for links to all major music platforms!",
     },
     // {
     //     header: "Phase 3 Groups Announced",
