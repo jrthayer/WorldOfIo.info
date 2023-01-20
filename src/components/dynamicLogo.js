742a2f131dcf915be0964389ccc1dc0a933dd64b
James Thayer <jrthayer@users.noreply.github.com>
2023-01-19 20:20:11 -0800

    Add dynamicLogo component
    +this component is a image component that maintains  aspect ratio and adjusts based on screen portrait/landscape.
    
    +doesn't ensure that the image won't overflow

---
+++ src/components/dynamicLogo.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import { Suspense } from "react";
+
+import styles from "./dynamicLogo.module.scss";
+
+function DynamicLogo(props) {
+    const [style, setStyle] = useState({});
+    const imgRef = useRef(null);
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        // Needed to avoid images starting at default prop values for 1 load
+        if (!loaded) {
+            setStyle({
+                ["--width"]: "auto",
+                ["--height"]: "auto",
+                ["--width-max"]: "auto",
+                ["--height-max"]: "auto",
+            });
+        } else {
+            setStyle({
+                ["--width"]: imgRef.current.naturalWidth + "px",
+                ["--height"]: imgRef.current.naturalHeight + "px",
+                ["--width-max"]: props.widthMax,
+                ["--height-max"]: props.heightMax,
+            });
+        }
+    }, [loaded]);
+
+    return (
+        <img
+            ref={imgRef}
+            src={props.src}
+            alt={props.alt}
+            style={style}
+            className={styles.image}
+            onLoad={() => setLoaded(true)}
+        />
+    );
+}
+
+DynamicLogo.defaultProps = {
+    src: "",
+    alt: "",
+    widthMax: "80vw",
+    heightMax: "80vh",
+};
+
+export default DynamicLogo;
