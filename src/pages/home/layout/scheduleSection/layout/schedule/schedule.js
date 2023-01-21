b33cbf5b0c8fea9246a42d7dad48f4c91a920a41
James Thayer <jrthayer@users.noreply.github.com>
2023-01-20 23:29:03 -0800

    Update schedule headers

--- src/pages/home/layout/scheduleSection/layout/schedule/schedule.js
+++ src/pages/home/layout/scheduleSection/layout/schedule/schedule.js
@@ -9,8 +9,8 @@ const Schedule = (props) => {
     const data = props.data;
     return (
         <div className={styles.container}>
-            <h2 className={`fs-650 ${styles.header}`}>Schedule</h2>
-            <h4 className={`${styles.week}`}>Week of Jan 16th</h4>
+            <h2 className={`fs-650 header-ioverse`}>Schedule</h2>
+            <h4 className={`header-ioverse-gradient`}>Week of Jan 16th</h4>
             <div className={styles.innerContainer}>
                 {data.map((day, index) => {
                     // if (day.events.length != 0) {
