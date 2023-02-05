adafb42d393a467dd8d4c8619e752d25d231d558
James Thayer <jrthayer@users.noreply.github.com>
2023-02-04 19:28:51 -0800

    Update misc css

--- src/components/scrollingScreen.js
+++ src/components/scrollingScreen.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import { useScroll } from "components/providers/scrollProvider";
 import { useViewport } from "components/providers/viewportProvider";
---
+++ src/data/faq.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        question: "YouTube",
+        answer: "https://www.youtube.com/channel/UCk_yrKoKi74gKCCWVRyvwYw",
+    },
+    {
+        question: "How do I use this page?",
+        answer: "Insert the rss link found on your patreon membership page in the field above and click submit.",
+    },
+];
