04b979c385f7569c015f32dcfec6565cf823b408
James Thayer <jrthayer@users.noreply.github.com>
2023-04-21 00:55:03 -0700

    Separates sessions into shows/playlists

--- src/data/shows.js
+++ src/data/shows.js
@@ -1,13 +1,419 @@
-export default[{
+export default[
+    {
+    title: "IOverse OSTs",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: true,
+    playlistRegex: [''],
+    playlists: [
+    {
+        title: "Ioverse OST",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]}, 
+    {
+        title: 'Podcasts',
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        playlistRegex: ['Arcane Academy', 'Miss Demeanor', 'All King', ''],
+        playlists: [
+        {
+            title: "Arcane Academy Podcasts",
+            numberOfEpisodes: 0,
+            duration: 0,
+            concluded: true,
+            sessions: []
+        },{
+            title: "Miss Demeanor Podcasts",
+            numberOfEpisodes: 0,
+            duration: 0,
+            concluded: true,
+            sessions: []
+        },{
+            title: "All King Podcasts",
+            numberOfEpisodes: 0,
+            duration: 0,
+            concluded: true,
+            sessions: []
+        },{
+            title: "Misc Podcasts",
+            numberOfEpisodes: 0,
+            duration: 0,
+            concluded: true,
+            sessions: []
+        }]
+    },
+    {
+    title: "Phase 2",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: true,
+    playlistRegex: [''],
+    playlists: [
+    {
+        title: "Phase 2 Master Playlist",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
     title: "Arcane Academy",
     numberOfEpisodes: 0,
     duration: 0,
     concluded: true,
-    playlistRegex: ['', ['S2, Season 2'], 'Astral Academy'],
-    playlists: [{
+    playlistRegex: ['Astral Academy', ['S2', 'Season 2'], ''],
+    playlists: [
+    {
+        title: "Astral Academy",
         numberOfEpisodes: 0,
         duration: 0,
         concluded: true,
-        episodes: []
-    }]
-},{}];
+        sessions: []
+    },{
+        title: "Arcane Academy Season 2",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    },{
+        title: "Arcane Academy Season 1",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
+    title: "Miss Demeanor",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: true,
+    playlistRegex: [['Nigel', 'Altered'],'S3', 'S2', ''],
+    playlists:[
+    {
+        title: "One Offs",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    },
+    {
+        title: "Miss Demeanor Season 3",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    },{
+        title: "Miss Demeanor Season 2",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    },{
+        title: "Miss Demeanor Season 1",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
+    title: "All King",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: true,
+    playlistRegex: ['Mainframe of Atlas','Radiance', 'Under IO', ''],
+    playlists:[
+    {
+        title: "Mainframe of Atlas (All King Season 4)",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    },{
+        title: "Radiance (All King Season 3)",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    },{
+        title: "Under IO (All King Season 2)",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    },{
+        title: "All King Season 1",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
+    title: "Goblins of IO",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: true,
+    playlistRegex: [''],
+    playlists: [
+    {
+        title: "Goblins of IO",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
+    title: "Tower of Archimedes",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: true,
+    playlistRegex: [''],
+    playlists: [
+    {
+        title: "Tower of Archimedes",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
+    title: "Hunters of IO",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: true,
+    playlistRegex: ['Spire of Euclid','Hunters of IO'],
+    playlists: [
+    {
+        title: "Spire of Euclid",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    },{
+        title: "Hunters of IO",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
+    title: "Bronn",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: true,
+    playlistRegex: [''],
+    playlists: [
+    {
+        title: "Bronn",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
+    title: "Altered Ballad",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: true,
+    playlistRegex: [''],
+    playlists: [
+    {
+        title: "Altered Ballad",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
+    title: "Nova Hellscape",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: true,
+    playlistRegex: [''],
+    playlists: [
+    {
+        title: "Nova Hellscape",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
+    title: "Eye of Asmodia",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: true,
+    playlistRegex: ['Shadow of Asmodia',''],
+    playlists: [
+    {
+        title: "Shadow of Asmodia",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    },
+    {
+        title: "Eye of Asmodia",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
+    title: "Phase 2 Major Events",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: true,
+    playlistRegex: ["The Shadow Invasion",'Chamber of the Eight', 'Kaasma Khara Final Battle'],
+    playlists: [
+    {
+        title: "The Shadow Invasion",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    },
+    {
+        title: "Chamber of the Eight",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    },
+    {
+        title: "Kaasma Khara Final Battle",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
+    title: "Phase 3",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: false,
+    playlistRegex: [''],
+    playlists: [
+    {
+        title: "Phase 3 Master Playlist",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: false,
+        sessions: []
+    }]},
+    {
+    title: "Pipe Dreamers",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: false,
+    playlistRegex: ['Season 3', ['Season 2', 'S2'], ''],
+    playlists: [
+    {
+        title: "Pipe Dreamers Season 3",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: false,
+        sessions: []
+    },
+    {
+        title: "Pipe Dreamers Season 2",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    },
+    {
+        title: "Pipe Dreamers Season 1",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
+    title: "Deadbeats",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: false,
+    playlistRegex: ['Season 3', ['Season 2' , 'S2'], ''],
+    playlists: [
+    {
+        title: "Deadbeats Season 3",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: false,
+        sessions: []
+    },
+    {
+        title: "Deadbeats Season 2",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    },
+    {
+        title: "Deadbeats Season 1",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
+    title: "Curious Curios",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: false,
+    playlistRegex: ['Season 3', ['Season 2' , 'S2'], ''],
+    playlists: [
+    {
+        title: "Curious Curios Season 3",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: false,
+        sessions: []
+    },
+    {
+        title: "Curious Curios Season 2",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    },
+    {
+        title: "Curious Curios Season 1",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
+    title: "March on Faewunder",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: true,
+    playlistRegex: [''],
+    playlists: [
+    {
+        title: "March on Faewunder Season 1",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]},
+    {
+    title: "Into The Shadow's Breach",
+    numberOfEpisodes: 0,
+    duration: 0,
+    concluded: true,
+    playlistRegex: [''],
+    playlists: [
+    {
+        title: "Into The Shadow's Breach Season 1",
+        numberOfEpisodes: 0,
+        duration: 0,
+        concluded: true,
+        sessions: []
+    }]}
+];
--- src/pages/rss/utils/parseRSS.js
+++ src/pages/rss/utils/parseRSS.js
@@ -13,22 +13,31 @@ import {default as shows} from "data/shows";
 
 
 export function convertArray(rawArray){
-    console.log(shows);
-    const convertedArray = rawArray.map((singleShow)=>{
+    rawArray.reverse();
+    let duplicateKey = 0;
+
+    const convertedArray = rawArray.map((singleShow, index)=>{
+        //Duplicate session name fix
+        if(singleShow.title.includes('Curios Season 3 Session 1')){
+            if(duplicateKey === 1){
+                singleShow.title = 'Curious Curios Season 3 Session 2 MP3';
+            }
+            duplicateKey++;
+        };
+
         //strip data
         const showData = stripData(singleShow);
 
         //add to shows
-        addToShows(shows, showData);
+        addToShows(shows, showData, index);
 
         //return converted show object
         return showData;
     });
 
     console.log(shows);
-    console.log(convertedArray);
-    //reverse array direction since the default rss order is newest to oldest.
-    return convertedArray.reverse();
+
+    return convertedArray;
 }
 
 function stripData(rawData){
@@ -44,27 +53,213 @@ function stripData(rawData){
     return showData;
 }
 
-function addToShows(shows, sessionData){
+function addToShows(shows, sessionData, sessionIndex){
     const title = sessionData.title;
+    let showFound = false;
 
-    shows.forEach((show, index, showArray)=>{
-        if(title.includes(show.title)){
-            showArray[index].numberOfEpisodes++;
-            console.log(`${shows[index].duration} -- ${sessionData.duration}`)
-            showArray[index].duration += sessionData.duration;
-            showArray[index].endDate = sessionData.airDate;
-            if (typeof showArray[index].startDate === 'undefined') {
-                showArray[index].startDate = sessionData.airDate;
+    if(sessionIndex<262){
+        let show = shows.find(playlist => playlist.title === "Phase 2");
+        updateShow(show, sessionIndex, sessionData);
+    }
+    else{
+        let show = shows.find(playlist => playlist.title === "Phase 3");
+        updateShow(show, sessionIndex, sessionData);
+    }
+
+    //check for deadbeats x pipe dreamers crossover
+    if(showFound!== true){
+        if(title.toLowerCase().includes('deadbeats x pipe dreamers')){
+            let show = shows.find(playlist => playlist.title === "Deadbeats");
+            showFound = updateShow(show, sessionIndex, sessionData, 'Season 2');
+
+            show = shows.find(playlist => playlist.title === "Pipe Dreamers");
+            showFound = updateShow(show, sessionIndex, sessionData, 'Season 2');
+        }
+    }
+
+    //check for OST
+    if(showFound!== true){
+        if(title.includes('IOverse OST')){
+            let podcasts = shows.find(playlist => playlist.title === "IOverse OSTs");
+            showFound = updateShow(podcasts, sessionIndex, sessionData);
+        }
+    }
+
+    //check for podcasts
+    if(showFound!== true){
+        if(title.includes('Podcast')){
+            let podcasts = shows.find(playlist => playlist.title === "Podcasts");
+            showFound = updateShow(podcasts, sessionIndex, sessionData);
+        }
+    }
+
+    //check for curious curious typos
+    if(showFound !== true){
+        if(title.includes('Curious Curious')){
+            let show = shows.find(show=>show.title === "Curious Curios");
+            if(title.includes('S2') || title.includes('Season 2')){
+                showFound = updateShow(show, sessionIndex, sessionData, "Season 2");
             }
+            else if(title.includes('Season 3')){
+                showFound = updateShow(show, sessionIndex, sessionData, "Season 3");
+            }
+        }
+    }
+
+    //check for oswin
+    if(showFound !== true){
+        if(title.includes('Oswin')){
+            let show = shows.find(show=>show.title === "Goblins of IO");
+            showFound = updateShow(show, sessionIndex, sessionData, "");
+        }
+    }
+
+    //check for weyzi
+    if(showFound !== true){
+        if(title.includes('Weyzi')){
+            let show = shows.find(show=>show.title === "Miss Demeanor");
+            showFound = updateShow(show, sessionIndex, sessionData, "S3");
+        }
+    }
+
+    //check for chamber
+    if(showFound !== true){
+        if(title.includes('Chamber of the Eight')){
+            let show = shows.find(show=>show.title === "Miss Demeanor");
+            showFound = updateShow(show, sessionIndex, sessionData, "S3");
+
+            show = shows.find(show=>show.title === "Goblins of IO");
+            showFound = updateShow(show, sessionIndex, sessionData, "");
+
+            show = shows.find(show=>show.title === "Phase 2 Major Events");
+            showFound = updateShow(show, sessionIndex, sessionData, "Chamber of the Eight");
+        }
+    }
+
+    //check for Kaasma Khara Fight
+    if(showFound !== true){
+        if(title.includes('Kaasma Khara Final Battle')){
+            let show = shows.find(show=>show.title === "Miss Demeanor");
+            showFound = updateShow(show, sessionIndex, sessionData, "S2");
+
+            show = shows.find(show=>show.title === "Goblins of IO");
+            showFound = updateShow(show, sessionIndex, sessionData, "");
+
+            show = shows.find(show=>show.title === "Arcane Academy");
+            showFound = updateShow(show, sessionIndex, sessionData, "Astral Academy");
+
+            show = shows.find(show=>show.title === "Phase 2 Major Events");
+            showFound = updateShow(show, sessionIndex, sessionData, "Kaasma Khara Final Battle");
+        }
+    }
+
+    //check for shadow invasion
+    if(showFound !== true){
+        if(title.includes('The Shadow Invasion')){
+            let show = shows.find(show=>show.title === "Phase 2 Major Events");
+            showFound = updateShow(show, sessionIndex, sessionData, "The Shadow Invasion");
+
+            show = shows.find(show=>show.title === "Bronn");
+            showFound = updateShow(show, sessionIndex, sessionData, "");
+
+            show = shows.find(show=>show.title === "Goblins of IO");
+            showFound = updateShow(show, sessionIndex, sessionData, "");
+
+            show = shows.find(show=>show.title === "Miss Demeanor");
+            showFound = updateShow(show, sessionIndex, sessionData, "S3");
+        }
+    }
+
+    //check for shows
+    if(showFound !== true){
+        shows.forEach((show)=>{
+            if(title.toLowerCase().includes(show.title.toLowerCase())){
+                showFound = updateShow(show, sessionIndex, sessionData);
+            }
+        })
+    }
+
+    //check arcane academy
+    if(showFound !== true){
+        if(title.includes('Astral Academy')){
+            let astralAcademy = shows.find(show=>show.title === "Arcane Academy");
+            showFound = updateShow(astralAcademy, sessionIndex, sessionData);
+        }
+    }
+
+    //check all king
+    if(showFound !== true){
+        const allKingSeasons = ["Under IO", "Radiance", "Mainframe of Atlas"];
+        allKingSeasons.some((seasonTitle)=>{
+            if(title.includes(seasonTitle)){
+                let allKing = shows.find(show => show.title === "All King");
+                return updateShow(allKing, sessionIndex, sessionData);
+            }else{
+                return false;
+            }
+        })
+    }
+
+    //check hunters
+    if(showFound !== true){
+        if(title.includes('Spire of Euclid')){
+            let astralAcademy = shows.find(show=>show.title === "Hunters of IO");
+            showFound = updateShow(astralAcademy, sessionIndex, sessionData);
+        }
+    }
+
+    //check asmodia
+    if(showFound !== true){
+        if(title.includes('Shadow of Asmodia')){
+            let asmodia = shows.find(show=>show.title === "Eye of Asmodia");
+            showFound = updateShow(asmodia, sessionIndex, sessionData);
+        }
+    }
+}
+
+function updateShow(show, sessionIndex, sessionData, title = sessionData.title){
+    const playlistIndex = determinePlaylistIndex(show.playlistRegex, title);
+
+    if(playlistIndex !== -1){
+        addSession(show, sessionData);
+        const playlist = show.playlists[playlistIndex];
+        addSession(playlist, sessionData);
+        playlist.sessions.push(sessionIndex);
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function addSession(show, data){
+    show.numberOfEpisodes++;
+    show.duration += data.duration;
+
+    if (typeof show.startDate === 'undefined') {
+        show.startDate = data.airDate;
+    }
+    show.endDate = data.airDate;
+}
 
 
-            // add to playlist
-            // determine season 
-            // add to season
+function determinePlaylistIndex(array, title){
+    let foundIndex = -1;
+
+    array.some((regex, index)=>{
+        if(Array.isArray(regex)){
+            const nestedIndex = determinePlaylistIndex(regex, title);
+
+            if(nestedIndex !== -1){
+                foundIndex = index;
+                return true;
+            }
+        }
+        else if(title.toLowerCase().includes(regex.toLowerCase())){
+            foundIndex = index;
+            return true;
         }
     })
 
-    //if playlist wasn't found check arcane academy
-
-    //if playlist still isn't found check all king
+    return foundIndex;
 }
