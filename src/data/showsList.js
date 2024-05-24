cdbac242df485d17d3bc978d8d59f11287624751
James Thayer <jrthayer@users.noreply.github.com>
2024-05-24 16:44:20 -0700

    Add data files for new RSS parsing code
    Previous version is very unorganized and hard to maintain

---
+++ src/data/showsList.js
@@ -0,0 +1,65 @@
+export default [
+    { title: "Phase 2" },
+    { title: "Phase 3", concluded: false },
+    {
+        title: "Phase 2 Major Events",
+        seasonMatchStrings: [
+            "Kaasma Khara Final Battle",
+            "Chamber of the Eight",
+            "The Shadow Invasion",
+        ],
+    },
+    {
+        title: "Arcane Academy",
+        seasonMatchStrings: ["", ["S2", "Season 2"], "Astral Academy"],
+    },
+    {
+        title: "Miss Demeanor",
+        seasonMatchStrings: [
+            "",
+            "S2",
+            ["S3", "Season 3", "Campaign Finale"],
+            ["Nigel", "Altered"],
+        ],
+    },
+    {
+        title: "All King",
+        seasonMatchStrings: ["", "Under IO", "Radiance", "Mainframe of Atlas"],
+    },
+    {
+        title: "Goblins of IO",
+    },
+    {
+        title: "Tower of Archimedes",
+    },
+    { title: "Hunters of IO", seasonMatchStrings: ["", "Spire of Euclid"] },
+    { title: "Bronn" },
+    { title: "Altered Ballad" },
+    { title: "Nova Hellscape" },
+    {
+        title: "Eye of Asmodia",
+        seasonMatchStrings: ["", "Shadow of Asmodia"],
+    },
+    {
+        title: "Pipe Dreamers",
+        seasonMatchStrings: ["", ["Season 2", "S2"], "Season 3"],
+    },
+    {
+        title: "Deadbeats",
+        concluded: false,
+        seasonMatchStrings: ["", ["Season 2", "S2"], "Season 3", "Season 4"],
+    },
+    {
+        title: "Curious Curios",
+        seasonMatchStrings: ["", ["Season 2", "S2"], "Season 3"],
+    },
+    { title: "March on Faewunder" },
+    {
+        title: "Into The Shadow's Breach",
+        concluded: false,
+        playlistRegex: ["", ["Season 2", "S2"], "Season 3"],
+    },
+    {
+        title: "Dawn of Ganymede",
+    },
+];
